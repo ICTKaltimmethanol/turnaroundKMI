640c1ec409c9303cc261919c179f65df97319e17
Elxixau <indra90078@gmail.com>
2025-12-24 11:20:44 +0800

    fix bug

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -13,47 +13,46 @@ class AccessController extends Controller
     }
 
     public function cek(Request $request)
-{
-    $request->validate([
-        'kode' => 'required|string'
-    ]);
+    {
+        $request->validate([
+            'kode' => 'required|string'
+        ]);
 
-    $kode = $request->kode;
+        $gates = [
+            'Gate 1' => ['GATE1TA2025'],
+            'Gate 2' => ['GATE2TA2025'],
+            'Gate 3' => ['GATE3TA2025'],
+        ];
 
-    $gates = [
-        'Gate 1' => ['GATE1TA2025'],
-        'Gate 2' => ['GATE2TA2025'],
-        'Gate 3' => ['GATE3TA2025'],
-    ];
+        foreach ($gates as $gate => $codes) {
+            if (in_array($request->kode, $codes)) {
 
-    foreach ($gates as $gate => $codes) {
-        if (in_array($kode, $codes)) {
+                $request->session()->regenerate();
 
-            $request->session()->regenerate();
-            session([
-                'gate' => $gate,
-                'gate_login_at' => now(),
-            ]);
+                session([
+                    'gate' => $gate,
+                    'gate_login_at' => now(),
+                ]);
 
-            return response()->json([
-                'success' => true,
-                'gate' => $gate,
-            ], 200);
+                return response()->json([
+                    'success' => true,
+                    'gate' => $gate,
+                ]);
+            }
         }
-    }
 
-    return response()->json([
-        'success' => false,
-        'message' => 'Kode tidak valid'
-    ], 401);
-}
+        return response()->json([
+            'success' => false,
+            'message' => 'Kode tidak valid'
+        ], 401);
+    }
 
 
 
     public function logout(Request $request)
     {
-        $request->session()->invalidate();      
-        $request->session()->regenerateToken(); 
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect()->route('akses.index');
     }
