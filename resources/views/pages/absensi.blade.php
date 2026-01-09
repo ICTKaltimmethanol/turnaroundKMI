b77fd0e32c2d1454b2cf1f1b402337c7bf53ca0f
Elxixau <indra90078@gmail.com>
2026-01-09 14:06:20 +0800

    add space time for scan by id

--- resources/views/pages/absensi.blade.php
+++ resources/views/pages/absensi.blade.php
@@ -188,7 +188,7 @@ function handleScan() {
             const bgColor =
                 data.status === 'success'
                     ? 'bg-green-100 border-green-400 text-green-700'
-                    : 'bg-red-100 border-red-400 text-red-700';
+                    : 'bg-red-100 border-red-400 text-yellow-700';
 
             document.getElementById('employeeData').innerHTML = `
                 <div class="${bgColor} border px-4 py-3 rounded mb-4">
