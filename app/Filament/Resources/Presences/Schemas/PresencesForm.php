b783d1e59dcc7d705921125510bb173ddd641a62
Elxixau <indra90078@gmail.com>
2026-01-16 10:10:37 +0800

    fix total_time calculation panel admin

--- app/Filament/Resources/Presences/Schemas/PresencesForm.php
+++ app/Filament/Resources/Presences/Schemas/PresencesForm.php
@@ -1,115 +1,69 @@
-    <?php
-    namespace App\Filament\Resources\Presences\Schemas;
+<?php
+namespace App\Filament\Resources\Presences\Schemas;
 
-    use Filament\Forms\Components\TextInput;
-    use Filament\Schemas\Components\Section;
-    use Filament\Forms\Components\DatePicker;
-    use Filament\Forms\Components\DateTimePicker;
-    use Filament\Forms\Components\TimePicker;
-    use Filament\Forms\Components\Select;
-    use App\Models\Company;
-    use Filament\Schemas\Schema;
-    use Carbon\Carbon;
-    use Filament\Schemas\Components\Utilities\Get;
-    use Filament\Schemas\Components\Utilities\Set;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Schemas\Schema;
+use Carbon\Carbon;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 
-    
-
-    class PresencesForm
+class PresencesForm
+{
+    public static function configure(Schema $schema): Schema
     {
-        public static function configure(Schema $schema): Schema
-        {
-            return $schema
-                ->components([
-                    TextInput::make('total_time')
-                        ->label('Total Waktu (Menit)')
-                        ->numeric()
-                        ->disabled()
-                        ->dehydrated(),
- 
+        return $schema->components([
+            TextInput::make('total_time')
+                ->label('Total Waktu (Menit)')
+                ->numeric()
+                ->disabled()
+                ->dehydrated(),
 
-                    /* TextInput::make('total_time')
-                        ->label('Total Waktu (Menit)')
-                        ->numeric()
-                        ->minValue(0)
+            Section::make('Presensi Waktu Masuk')
+                ->relationship('presenceIn')
+                ->schema([
+                    DatePicker::make('presence_date')
+                        ->label('Tanggal Masuk')
+                        ->required()
                         ->live()
-                        ->afterStateUpdated(fn ($state, $set) =>
-                            $set('total_time', abs((int) $state))
-                        )
-                        ->dehydrated(), */
+                        ->afterStateUpdated(fn (Get $get, Set $set) =>
+                            self::generateTotalMinute($get, $set)
+                        ),
 
-                    Select::make('employees_id')
-                        ->relationship('employee', 'full_name')
-                        ->getOptionLabelFromRecordUsing(
-                            fn ($record) => $record->full_name ?? '-'
-                        )
-                        ->label('Nama Pekerja')
-                        ->disabled()
-                        ->searchable()
-                        ->preload(),
+                    TimePicker::make('presence_time')
+                        ->label('Waktu Masuk')
+                        ->required()
+                        ->live()
+                        ->afterStateUpdated(fn (Get $get, Set $set) =>
+                            self::generateTotalMinute($get, $set)
+                        ),
+                ]),
 
-                    Select::make('company_id')
-                        ->label('Perusahaan')
-                        ->relationship('company', 'name', modifyQueryUsing: fn ($query) => $query->orderBy('name'))
-                        ->searchable()
-                        ->preload() 
-                        ->required(),
-                    
-                    Select::make('position_id')
-                        ->label('Posisi')
-                        ->relationship('position', 'name', modifyQueryUsing: fn ($query) => $query->orderBy('name'))
-                        ->searchable()
-                        ->preload()
-                        ->required(),
+            Section::make('Presensi Waktu Pulang')
+                ->relationship('presenceOut')
+                ->schema([
+                    DatePicker::make('presence_date')
+                        ->label('Tanggal Pulang')
+                        ->required()
+                        ->live()
+                        ->afterStateUpdated(fn (Get $get, Set $set) =>
+                            self::generateTotalMinute($get, $set)
+                        ),
 
-                Section::make('Presensi Waktu Masuk')
-                    ->relationship('presenceIn')
-                    ->schema([
-                        DatePicker::make('presence_date')
-                            ->label('Tanggal Masuk')
-                            ->live()
-                            ->required(),
+                    TimePicker::make('presence_time')
+                        ->label('Waktu Pulang')
+                        ->required()
+                        ->live()
+                        ->afterStateUpdated(fn (Get $get, Set $set) =>
+                            self::generateTotalMinute($get, $set)
+                        ),
+                ]),
+        ]);
+    }
 
-                        TimePicker::make('presence_time')
-                            ->label('Waktu Masuk')
-                            ->live()
-                            ->afterStateUpdated(
-                                fn( Get $get, Set $set)=>
-                                self::generateTotalMinute($get,$set)
-                            )
-                            ->required(),
-                    ]),
-
-                Section::make('Presensi Waktu Pulang')
-                    ->relationship('presenceOut')
-                    ->schema([
-                        DatePicker::make('presence_date')
-                            ->label('Tanggal Pulang')
-                            ->required()
-                            ->live()
-                            ->afterStateUpdated(
-                                fn( Get $get, Set $set) =>
-                                self::generateTotalMinute($get,$set)
-                            )
-                            //->reactive()
-                            ,
-
-                        TimePicker::make('presence_time')
-                            ->label('Waktu Pulang')
-                            ->live()
-                            ->afterStateUpdated(
-                                fn( Get $get, Set $set) =>
-                                self::generateTotalMinute($get,$set)
-                            )
-                            ->afterStateHydrated(
-                                fn(Get $get, Set $set) =>
-                                self::generateTotalMinute($get,$set)
-                            ),//->reactive()
-                    ]),
-                ]);
-        }
-        
-        
     protected static function generateTotalMinute(Get $get, Set $set): void
     {
         $inDate  = $get('../../presenceIn.presence_date');
@@ -124,18 +78,10 @@
         $start = Carbon::parse("$inDate $inTime");
         $end   = Carbon::parse("$outDate $outTime");
 
-        // SHIFT MALAM (pulang lebih kecil dari masuk)
         if ($end->lessThan($start)) {
             $end->addDay();
         }
 
-        $minutes = $start->diffInMinutes($end);
-
-        // pastikan tidak minus
-        $set('../../total_time', abs($minutes));
+        $set('../../total_time', $start->diffInMinutes($end));
     }
-
-
-        
-    }
-
+}
