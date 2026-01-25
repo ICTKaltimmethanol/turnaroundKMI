4f9faea5c1cc25950a12c7ecde53d80317b7dee2
Elxixau <indra90078@gmail.com>
2026-01-25 11:39:30 +0800

    update feature

--- app/Filament/Resources/Presences/Pages/CreatePresences.php
+++ app/Filament/Resources/Presences/Pages/CreatePresences.php
@@ -17,29 +17,37 @@ class CreatePresences extends CreateRecord
     }
 
     protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $presenceIn = \App\Models\PresenceIn::create([
-            'employees_id' => $data['employees_id'],
-            'presence_date' => $data['presenceIn']['presence_date'],
-            'presence_time' => $data['presenceIn']['presence_time'],
+{
+    // === SIMPAN PRESENCE IN ===
+    $presenceIn = \App\Models\PresenceIn::create([
+        'employees_id'  => $data['employees_id'],
+        'presence_date' => $data['presenceIn']['presence_date'],
+        'presence_time' => $data['presenceIn']['presence_time'],
+    ]);
+
+    // === SIMPAN PRESENCE OUT (OPTIONAL) ===
+    $presenceOutId = null;
+
+    if (
+        !empty($data['presenceOut']['presence_date']) &&
+        !empty($data['presenceOut']['presence_time'])
+    ) {
+        $presenceOut = \App\Models\PresenceOut::create([
+            'employees_id'  => $data['employees_id'],
+            'presence_date' => $data['presenceOut']['presence_date'],
+            'presence_time' => $data['presenceOut']['presence_time'],
         ]);
 
-        $presenceOutId = null;
-
-        if (!empty($data['presenceOut']['presence_date']) && !empty($data['presenceOut']['presence_time'])) {
-            $presenceOut = \App\Models\PresenceOut::create([
-                'employees_id' => $data['employees_id'],
-                'presence_date' => $data['presenceOut']['presence_date'],
-                'presence_time' => $data['presenceOut']['presence_time'],
-            ]);
-            $presenceOutId = $presenceOut->id;
-        }
-
-        return [
-            ...$data,
-            'presenceIn_id' => $presenceIn->id,
-            'presenceOut_id' => $presenceOutId,
-        ];
+        $presenceOutId = $presenceOut->id;
     }
+
+    // === KEMBALIKAN DATA UNTUK TABEL PRESENCES ===
+    return [
+        ...$data,
+        'presenceIn_id'  => $presenceIn->id,
+        'presenceOut_id' => $presenceOutId,
+    ];
+}
+
 
 }
